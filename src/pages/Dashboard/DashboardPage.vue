ae6607a728a152d483cca9dbca75ef8c601fba2d
John Daniel Del Monte <delmontejohndaniel80@gmail.com>
2024-02-22 16:46:12 +0800

    done making the dashboard page

--- src/pages/Dashboard/DashboardPage.vue
+++ src/pages/Dashboard/DashboardPage.vue
@@ -1,5 +1,17 @@
 <template>
   <q-page>
-    <h2 class="menu-title q-ma-none">Hello World</h2>
+    <h2 class="menu-title q-ma-none">Dashboard</h2>
+    <q-list class="row">
+      <q-item v-for="i in 4" :key="i" class="col-3 q-pl-none">
+        <q-card class="card full-width">
+          <q-card-section class="card__title text-weight-bold">Card {{ i }}</q-card-section>
+          <q-card-section class="card__description">Description</q-card-section>
+        </q-card>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
+
+<style scoped lang="scss">
+@import './styles/DashboardPage.scss';
+</style>
